6049d6899b157ae6c76ac06401c41c0f4a5c3b39
keh2dev <124110845+keh2dev@users.noreply.github.com>
2023-02-02 17:33:02 +0900

    Add files via upload

---
+++ day04/out.js
@@ -0,0 +1,27 @@
+// 함수는 여러 절차가 있는 처리를 이름을 부여하여 기능을 정의!
+
+function 여행하다() {
+    document.write("out.js를 통해 여행을 할거야<br>")
+}
+
+function 쉬다() {
+    document.write("out.js를 통해 쉴거야<br>")
+}
+
+// function 빨래하다() {
+//     console.log("1. 먼지를 털자.")
+//     console.log("2. 물에 집어 넣는다.")
+//     console.log("3. 비누를 바른다.")
+//     console.log("4. 오물조물 비빈다.")
+//     console.log("5. 물에 여러번 헹군다.")
+//     console.log("6. 짠다.")
+//     console.log("7. 넌다.")
+// }
+
+// //청소하다에 대한 함수 정의해서 사용해보세요
+// function 청소하다() {
+//     console.log("1. 쓸고")
+//     console.log("2. 닦고")
+//     console.log("3. 광내고")
+// }
+
