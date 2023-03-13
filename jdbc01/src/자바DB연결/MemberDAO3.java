c23b93be4739db9223d50092774569513482a439
keh2dev <124110845+keh2dev@users.noreply.github.com>
2023-03-13 17:52:22 +0900

    Add files via upload

--- jdbc01/src/자바DB연결/MemberDAO3.java
+++ jdbc01/src/자바DB연결/MemberDAO3.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Locale;
 
 import 화면DB연결.MemberVO;
@@ -14,6 +15,69 @@ public class MemberDAO3 {
 	// 메서드를 정의했다고 실행되는 것은 아니다!
 	// 메서드를 쓰는 것 ==> 메서드 호출(call)!
 
+	public ArrayList<MemberVO> list()  {
+		
+		ResultSet rs = null;
+		ArrayList<MemberVO> list = new ArrayList<>();
+		MemberVO bag = null;
+		
+		try {
+			// 1.오라클 11g와 연결한 부품 설정
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("1. 오라클과 자바 연결할 부품 설정 성공.");
+			Locale.setDefault(Locale.US); // 맥 locale에러나신 분들만!!!
+
+			// 2.오라클 11g에 연결해보자.(java --- oracle)
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String user = "system";
+			String password = "oracle";
+			Connection con = DriverManager.getConnection(url, user, password); // Connection
+			// String data = JOptionPane.showInputDialog("이름입력"); //String, 임아무개
+			System.out.println("2. 오라클 연결 성공.");
+
+			// ipaddress ==> InetAddress
+			// String url = "http://wwww.naver.com";
+			// URL u = new URL(url);
+			// 자바는 부품조립식이여서,String에 넣은 문자열을 특정한 부품으로 인식하지 못함.
+			// 특정한 부품으로 인식하려면 그 부품으로 만들어주어야 한다.
+			// SQL부품으로 만들어주어야 함.
+			// PreparedStatement가 SQL부품!!
+
+			String sql = "select * from hr.MEMBER";
+			PreparedStatement ps = con.prepareStatement(sql); // PreparedStatement
+			//ps.setString(1, id);
+			System.out.println("3. SQL문 부품(객체)으로 만들어주기 성공.");
+
+			rs = ps.executeQuery();// select문 전송시 결과가 테이블형태라서 변경.
+			System.out.println("4. SQL문 오라클로 보내기 성공.");
+			while (rs.next()) {//1.rs.next가 참이면
+				//System.out.println("검색결과있음. 성공.");
+				//2. 각 컬럼에서 값들을 꺼내오자
+				String id2 = rs.getString(1); // id
+				String pw = rs.getString(2); // pw
+				String name = rs.getString(3); // name
+				String tel = rs.getString(4); // tel
+				System.out.println(id2 + " " + pw + " " + name + " " + tel);
+				//3. 가방을 만들자.
+				bag = new MemberVO();
+				bag.setId(id2);
+				bag.setName(name);
+				bag.setPw(pw);
+				bag.setTel(tel);
+				//4. list에 bag을 추가해주자.
+				list.add(bag);
+			} 
+
+			// System.out.println(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//return id, pw, name, tel은 XXXXX!
+		//return 뒤에는 반드시 여러 데이터를 묶어서 리턴해주어야 함.
+		return list;
+
+	}
+	
 	public int login(MemberVO bag) {
 		int result = 0;
 		ResultSet rs = null;
