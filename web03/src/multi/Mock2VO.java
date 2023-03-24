0f3e92ec95a7b222d1e1ce81ca9bf5851ad7784d
keh2dev <124110845+keh2dev@users.noreply.github.com>
2023-03-24 17:56:12 +0900

    Add files via upload

---
+++ web03/src/multi/Mock2VO.java
@@ -0,0 +1,46 @@
+package multi;
+
+public class Mock2VO {
+	private int id;
+	private String name;
+	private String company;
+	private String code;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	@Override
+	public String toString() {
+		return "Mock2VO [id=" + id + ", name=" + name + ", company=" + company + ", code=" + code + "]";
+	}
+
+}
